5bea714118797135169de0c25abe58db56d2c058
JeromeTesselaar <jerome.tesselaar@hva.nl>
2023-03-10 07:42:43 +0100

    Feature: Implemented a Login Screen to the application

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form @submit.prevent="login">
+                  <v-text-field
+                    label="Email"
+                    type="email"
+                    v-model="email"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    required
+                  ></v-text-field>
+                  <v-btn color="primary" type="submit">Login</v-btn>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Add login logic here
+      console.log(`Email: ${this.email}, Password: ${this.password}`);
+    },
+  },
+};
+</script>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,34 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
-    </v-app-bar-title>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon @click="toggleSidebar"></v-app-bar-nav-icon>
+    <v-toolbar-title>Cloud Hosting Provider</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu offset-y>
+      <template #activator="{ on }">
+        <v-avatar size="32">
+          <v-icon>mdi-account-circle</v-icon>
+        </v-avatar>
+      </template>
+      <v-list>
+        <v-list-item @click="logout">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
-<script lang="ts" setup>
-  //
+<script>
+  export default {
+  methods: {
+    toggleSidebar() {
+      // Add logic to toggle sidebar here
+      console.log("Toggle sidebar");
+    },
+    logout() {
+      // Add logout logic here
+      console.log("Logout");
+    },
+  },
+};
 </script>
